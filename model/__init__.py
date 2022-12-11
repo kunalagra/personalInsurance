f7213e94d6a36f7f951d4e7619e41477648d4765
kunalrag <goku.agra@yahoo.in>
2022-12-11 16:19:25 +0530

    Make Deployment Ready

--- model/__init__.py
+++ model/__init__.py
@@ -1,26 +1,8 @@
 
 from app import app
 
-import pandas as pd
-import numpy as np
-import seaborn as sns
-from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
-from sklearn.model_selection import cross_val_score
-sns.set()
-import warnings
-warnings.filterwarnings('ignore')
-data = pd.read_csv("static\\insurance.csv")
-catColumns = ['sex', 'smoker', 'region']
-data_dum = pd.get_dummies(data, columns = catColumns, drop_first=True)
-x = data_dum[['age', 'bmi', 'smoker_yes','sex_male']]
-y = data_dum['charges']
-linreg = LinearRegression()
-ridgereg = RidgeCV(alphas=(0.1,0.3,0.7,1.0),cv=5)
-lassoreg = LassoCV(eps=0.001, cv = 5)
-linreg.fit(x,y)
-linreg.score(x,y)
-linreg.coef_
-linreg.intercept_
+linreg_intercept_ = -11633.494594179414
+linreg_coef_ = [259.45318189,323.0510586,23833.87003847,-109.04110867]
 
 def calc_insurance(age, bmi, smoking,sex):
     y = ((age*linreg.coef_[0]) + (bmi*linreg.coef_[1]) + (smoking*linreg.coef_[2]) + (sex*linreg.coef_[3]) - linreg.intercept_)
